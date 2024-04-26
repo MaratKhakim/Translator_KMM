d077a649406456259ae1819a0b120157a622ac5d
Marat <marat@justdice.io>
2024-04-26 11:47:59 +0200

    Shared View Model

--- shared/src/commonMain/kotlin/com/plcoding/translator_kmm/core/domain/util/CommonMutableStateFlow.kt
+++ shared/src/commonMain/kotlin/com/plcoding/translator_kmm/core/domain/util/CommonMutableStateFlow.kt
@@ -2,6 +2,5 @@ package com.plcoding.translator_kmm.core.domain.util
 
 import kotlinx.coroutines.flow.MutableStateFlow
 
-expect class CommonMutableStateFlow<T>(flow: MutableStateFlow<T>) : MutableStateFlow<T>
-
 fun <T> MutableStateFlow<T>.toCommonMutableStateFlow() = CommonMutableStateFlow(this)
+expect class CommonMutableStateFlow<T>(flow: MutableStateFlow<T>) : MutableStateFlow<T>
--- shared/src/commonMain/kotlin/com/plcoding/translator_kmm/core/domain/util/CommonStateFlow.kt
+++ shared/src/commonMain/kotlin/com/plcoding/translator_kmm/core/domain/util/CommonStateFlow.kt
@@ -2,6 +2,5 @@ package com.plcoding.translator_kmm.core.domain.util
 
 import kotlinx.coroutines.flow.StateFlow
 
-expect open class CommonStateFlow<T>(flow: StateFlow<T>): StateFlow<T>
-
 fun <T> StateFlow<T>.toCommonStateFlow() = CommonStateFlow(this)
+expect open class CommonStateFlow<T>(flow: StateFlow<T>): StateFlow<T>
